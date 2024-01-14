b65f102f0f74a8607ac0b28a0887d5e93b63a965
VarunSinghProjects <varunarunkumarsingh@gmail.com>
2024-01-14 17:24:20 +0530

    cors updated

--- index.js
+++ index.js
@@ -16,9 +16,17 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json()); //This allows the application to handle JSON data in request bodies.
 
-app.get("/", (req, res) => {
-  res.send("HI.. Welcome");
+router.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
 });
+
 app.use("/api/user", userRouter);
 
 app.listen(process.env.PORT || 5001, () => {
