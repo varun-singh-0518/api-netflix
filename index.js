ef8cab068ed0b82c5841a48a1dcb27003a8e73c2
VarunSinghProjects <varunarunkumarsingh@gmail.com>
2024-01-14 17:18:49 +0530

    cors added

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json()); //This allows the application to handle JSON data in request bodies.
 
 app.get("/", (req, res) => {
